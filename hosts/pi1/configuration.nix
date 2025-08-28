c8cb789f947e7a6febe41c4bb38e53174ea74b6e
Jonas Wittbrodt <jwittbrodt@posteo.de>
2025-08-28 18:24:39 +0200

    add autoupgrade

--- hosts/pi1/configuration.nix
+++ hosts/pi1/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
@@ -17,8 +13,14 @@
   boot.loader.generic-extlinux-compatible.enable = true;
 
   networking.hostName = "nixpi1";
-  my.sshServer.enable = true;
   security.sudo.wheelNeedsPassword = false;
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = true;
+    dates = "02:30";
+    flake = "github:jwittbrodt/nix-setup";
+  };
 
+  my.sshServer.enable = true;
   my.dnsServer.enable = true;
 }
