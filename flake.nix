203ccc2f83f74981d85275e59394e8597d0e4cf9
Jonas Wittbrodt <jwittbrodt@posteo.de>
2025-09-05 15:38:06 +0200

    add home-manager

--- flake.nix
+++ flake.nix
@@ -1,7 +1,15 @@
 {
   description = "My nix setup";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations = {
       nixpi1 = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
@@ -11,6 +19,11 @@
       aarch64Image = nixpkgs.lib.nixosSystem {
         modules = [ ./images/aarch64-sd.nix ];
       };
+    };
+
+    homeConfigurations."jonasw@JonasThinkpad" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./modules/home-manager ];
     };
 
     images.rpi4 = self.nixosConfigurations.aarch64Image.config.system.build.sdImage;
