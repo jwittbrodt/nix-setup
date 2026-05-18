22038ca1a99f19197ad90d05b6dd495f1ae5d6df
Jonas Wittbrodt <36197762+jwittbrodt@users.noreply.github.com>
2026-05-18 10:52:13 +0200

    upgrade nvidia drivers

--- hosts/Lenovo-PW03KNKH.nix
+++ hosts/Lenovo-PW03KNKH.nix
@@ -11,7 +11,7 @@
 
   targets.genericLinux.gpu.nvidia = {
     enable = true;
-    version = "580.126.09";
-    sha256 = "sha256-TKxT5I+K3/Zh1HyHiO0kBZokjJ/YCYzq/QiKSYmG7CY=";
+    version = "580.142";
+    sha256 = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
   };
 }
